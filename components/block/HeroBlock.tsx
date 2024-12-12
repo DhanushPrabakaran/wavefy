08fc718e5c61e80fe9a36aaf25affd7433eb2b7b
dhanush prabakaran <dhanushprabakaran18@gmail.com>
2024-12-12 22:37:30 +0530

    updateed front page design

--- components/block/HeroBlock.tsx
+++ components/block/HeroBlock.tsx
@@ -1,29 +1,40 @@
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Photo from "@/public/themes/SolarBlackTheme.png";
 const HeroBlock = () => {
   return (
-    <div className="flex  items-center  bg-fixed bg-no-repeat bg-cover flex-col text-center  min-h-screen align-middle backdrop-blur-2xl justify-around">
+    <div className="overflow-hidden ">
       <div className="">
-        <h1 className=" overflow-hidden p-2 uppercase  leading-none bg-clip-text bg-fixed bg-no-repeat font-antonsc  max-md:text-[10vw] text-[8vw]">
-          Click on
-          <span className=" text-green-600">
-            {" "}
-            SIGNUP.
-            <br />
-          </span>
-          Update YOur
-          <span className=" text-red-600">
-            {" "}
-            Details, <br />
-          </span>
-          choose the
-          <span className=" text-orange-600"> Theme </span>And
-          <br />
-          press publish
-          <span className=" text-amber-600">
-            {" "}
-            PUBLISH.
-            <br />
-          </span>
-        </h1>
+        <div className="mx-auto flex max-w-5xl flex-col items-center">
+          <div className="z-10 items-center text-center">
+            <h1 className="mb-8 font-antonsc text-pretty text-4xl font-medium lg:text-8xl">
+              Build your Own Portfolio With portfolioforge
+            </h1>
+            <p className="mx-auto max-w-screen-md text-muted-foreground lg:text-xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Elig
+              doloremque mollitia fugiat omnis! Porro facilis quo animi
+              consequatur. Explicabo.
+            </p>
+            <div className="mt-12 flex w-full flex-col justify-center gap-2 sm:flex-row">
+              <Button>
+                Get started now
+                <ChevronRight className="ml-2 h-4" />
+              </Button>
+              <Button variant="ghost">
+                Learn more
+                <ChevronRight className="ml-2 h-4" />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <Image
+          src={Photo}
+          alt="placeholder"
+          width={2000}
+          height={2000}
+          className="mx-auto mt-24 max-h-[700px] w-full max-w-7xl rounded-t-lg object-cover shadow-lg"
+        />
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,6 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        home: "url('../public/home.jpg')",
-      },
       fontFamily: {
         roboto: ["var(--font-robotorondensed)"],
         sans: ["var(--font-geist-sans)"],
@@ -67,6 +64,28 @@ export default {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
